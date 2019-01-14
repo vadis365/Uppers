8ec73c8f29a1323dd40d6b35715f7d25f5d16b5d
vadis365 <curve_linear@hotmail.com>
2019-01-14 00:30:36 +0000

    still not there

--- src/main/java/uppers/Uppers.java
+++ src/main/java/uppers/Uppers.java
@@ -2,11 +2,14 @@ package uppers;
 
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Reference.MOD_ID)
-
 public class Uppers {
+		public Uppers() {
+			MinecraftForge.EVENT_BUS.register(this);
+		}
 	 public static ItemGroup TAB = new ItemGroup(Reference.MOD_ID) {
 		 @Override
 		 public ItemStack createIcon() {
