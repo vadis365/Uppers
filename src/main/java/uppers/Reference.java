776ed76aeeb9033c62added250bfa936d126a5e3
vadis365 <curve_linear@hotmail.com>
2019-01-14 22:29:41 +0000

    dunno

--- src/main/java/uppers/Reference.java
+++ src/main/java/uppers/Reference.java
@@ -3,6 +3,6 @@ package uppers;
 public class Reference {
 	public static final String MOD_ID = "uppers";
 	public static final String MOD_NAME = "uppers";
-	public static final String VERSION = "0.3.13";
+	public static final String VERSION = "0.1.7";
 	public static final String UPPER = "upper";
 }
