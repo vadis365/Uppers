13499454e2c93b0a842b8e40c379c83f6ec322f5
vadis365 <curve_linear@hotmail.com>
2019-09-08 19:55:24 +0100

    fixed the stupid abstaction layer

--- src/main/java/uppers/tiles/IUpper.java
+++ src/main/java/uppers/tiles/IUpper.java
@@ -1,14 +1,11 @@
 package uppers.tiles;
 
-import javax.annotation.Nullable;
-
 import net.minecraft.block.Block;
-import net.minecraft.inventory.IInventory;
+import net.minecraft.tileentity.IHopper;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
-import net.minecraft.world.World;
 
-public interface IUpper extends IInventory {
+public interface IUpper extends IHopper  {
 	VoxelShape INSIDE_BOWL_SHAPE = Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 5.0D, 14.0D);
 	VoxelShape BLOCK_BELOW_SHAPE = Block.makeCuboidShape(0.0D, -16.0D, 0.0D, 16.0D, 0.0D, 16.0D);
 	VoxelShape COLLECTION_AREA_SHAPE = VoxelShapes.or(INSIDE_BOWL_SHAPE, BLOCK_BELOW_SHAPE);
@@ -16,13 +13,4 @@ public interface IUpper extends IInventory {
 	default VoxelShape getCollectionArea() {
 		return COLLECTION_AREA_SHAPE;
 	}
-
-	@Nullable
-	World getWorld();
-
-	double getXPos();
-
-	double getYPos();
-
-	double getZPos();
 }
