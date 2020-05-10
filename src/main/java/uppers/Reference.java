c2a4e94089c558eeba08c9d6739bb56ff56a66f1
vadis365 <curve_linear@hotmail.com>
2020-05-10 21:34:56 +0100

    1.15.2 stuff

--- src/main/java/uppers/Reference.java
+++ src/main/java/uppers/Reference.java
@@ -3,6 +3,6 @@ package uppers;
 public class Reference {
 	public static final String MOD_ID = "uppers";
 	public static final String MOD_NAME = "uppers";
-	public static final String VERSION = "0.2.11";
+	public static final String VERSION = "0.2.12";
 	public static final String UPPER = "upper";
 }
