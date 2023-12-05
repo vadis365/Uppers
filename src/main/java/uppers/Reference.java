3e76eb5b9c18cbc27d2ae44992cc00a3433f4fd3
vadis365 <6518708+vadis365@users.noreply.github.com>
2023-12-05 20:41:54 +0000

    meh

--- src/main/java/uppers/Reference.java
+++ src/main/java/uppers/Reference.java
@@ -3,6 +3,6 @@ package uppers;
 public class Reference {
 	public static final String MOD_ID = "uppers";
 	public static final String MOD_NAME = "uppers";
-	public static final String VERSION = "0.5.1";
+	public static final String VERSION = "1.0.0";
 	public static final String UPPER = "upper";
 }
